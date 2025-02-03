43391a22785d44dec48ac2d452f5ba73c8858778
théoo <90846584+theokbokki@users.noreply.github.com>
2025-02-03 09:27:20 +0100

    Add padding bottom to page

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -34,7 +34,7 @@
     </head>
     <body class="max-w-640 px-24 pt-80 h-screen mx-auto transition-all duration-300 border-stone-200 min-[672]:border-x" data-page="{{ $page }}">
         <script>document.body.classList.add('opacity-0', 'translate-y-16');</script>
-        <main class="relative">
+        <main class="relative pb-80">
             {{ $slot }}
         </main>
     </body>
