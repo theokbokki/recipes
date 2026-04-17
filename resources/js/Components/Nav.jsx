df4c2f09a025e36fab619fb02dac89271a01476f
théoo <90846584+theokbokki@users.noreply.github.com>
2026-04-17 09:15:51 +0200

    Add wrapper around nav

--- resources/js/Components/Nav.jsx
+++ resources/js/Components/Nav.jsx
@@ -18,13 +18,15 @@ export default function Nav() {
 
     return (
         <nav ref={navRef} className="nav nav--desktop">
-            <h2 className="sro">Main navigation</h2>
-            <div className="nav__left">
-                <Link href="/recipes" className="nav__link">Recipes</Link>
-                <Link href="/ingredients" className="nav__link">Ingredients</Link>
-            </div>
-            <div className="nav__right">
-                <Link href="/groceries" className="nav__link">Groceries</Link>
+            <div className="wrapper nav__wrapper">
+                <h2 className="sro">Main navigation</h2>
+                <div className="nav__left">
+                    <Link href="/recipes" className="nav__link">Recipes</Link>
+                    <Link href="/ingredients" className="nav__link">Ingredients</Link>
+                </div>
+                <div className="nav__right">
+                    <Link href="/groceries" className="nav__link">Groceries</Link>
+                </div>
             </div>
         </nav>
     );
